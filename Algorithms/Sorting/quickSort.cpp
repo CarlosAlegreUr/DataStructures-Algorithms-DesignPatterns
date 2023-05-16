80a466c005a0f7198295134aee6a565f24496ea1
CarlosAlegreUr <CarlosAlegreUr@users.noreply.github.com>
2023-05-16 11:28:32 +0200

    quickSort

--- Algorithms/Sorting/quickSort.cpp
+++ Algorithms/Sorting/quickSort.cpp
@@ -1,22 +1,59 @@
 #include <iostream>
 #include <vector>
 
-// TODO: Implement the partition function here
+void swap(std::vector<int> &arr, int index1, int index2)
+{
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
 
-// TODO: Implement the quicksort function here
+int partition(std::vector<int> &arr, int low, int high)
+{
+    int pivot = arr[high];  
+    int i = (low - 1);  
+ 
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;  
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, high);
+    return (i + 1);
+}
 
-void printArray(const std::vector<int>& arr) {
+void quickSortRec(std::vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        quickSortRec(arr, low, pivot - 1);
+        quickSortRec(arr, pivot + 1, high);
+    }
+}
+
+void quickSort(std::vector<int> &arr)
+{
+    quickSortRec(arr, 0, arr.size() - 1);
+}
+
+void printArray(const std::vector<int> &arr)
+{
     for (int i = 0; i < arr.size(); i++)
         std::cout << arr[i] << " ";
     std::cout << "\n";
 }
 
-int main() {
+int main()
+{
     std::vector<int> arr = {10, 7, 8, 9, 1, 5};
     std::cout << "Unsorted array is: ";
     printArray(arr);
 
-    // TODO: Call your quicksort function here
+    quickSort(arr);
 
     std::cout << "Sorted array is: ";
     printArray(arr);
