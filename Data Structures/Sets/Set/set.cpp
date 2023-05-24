cfe1399ea0a7d4fa2305b121904baeb0ca2704c8
CarlosAlegreUr <CarlosAlegreUr@users.noreply.github.com>
2023-05-24 21:01:19 +0200

    set implmeneted

--- Data Structures/Sets/Set/set.cpp
+++ Data Structures/Sets/Set/set.cpp
@@ -1,21 +1,39 @@
 #include "set.h"
+#include <algorithm>
 
 Set::Set() {}
 
 Set::~Set() {}
 
-bool Set::insert(int val) {
-    // implementation goes here
+bool Set::insert(int val)
+{
+    if (this->hash_map[val] == false)
+    {
+        this->keys.push_back(val);
+        this->hash_map[val] = true;
+        return true;
+    }
+    return false;
 }
 
-bool Set::remove(int val) {
-    // implementation goes here
+bool Set::remove(int val)
+{
+    if (this->hash_map[val] == true)
+    {
+        std::vector<int>::const_iterator index = std::find(this->keys.begin(), this->keys.end(), val);
+        this->keys.erase(index);
+        this->hash_map[val] = false;
+        return true;
+    }
+    return false;
 }
 
-bool Set::contains(int val) {
-    // implementation goes here
+bool Set::contains(int val)
+{
+    return this->hash_map[val];
 }
 
-std::size_t Set::size() const {
-    // implementation goes here
+std::size_t Set::size() const
+{
+    return this->keys.size();
 }
